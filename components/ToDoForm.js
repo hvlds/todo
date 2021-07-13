f3693a65fa7db0669e8129cd0a5027fea366cc7a
Hernán Valdés <hfvaldesg@gmail.com>
2021-07-13 18:51:58 +0200

    Small changes in style and displayed information.
    Start adding tags and passing the date of the creation of the task

--- components/ToDoForm.js
+++ components/ToDoForm.js
@@ -5,25 +5,30 @@ app.component("todo-form", {
         <h1 class="title is-offset-one-quarter has-text-centered">ToDo</h1>
         <form class="box" @submit.prevent="onSubmit">
             <div class="column columns">
-                <input class="input" v-model="task" type="text" id="task" placeholder="New Task">
+                <input class="input" v-model="taskDescription" type="text" id="taskDescription" placeholder="New Task">
             </div>
         </form>
         <todo-list v-bind:tasks="tasks"></todo-list>
     `,
     data() {
         return {
-            task: "",
+            taskDescription: "",
             tasks: [],
         }
     },
     methods: {
         onSubmit() {
-            if (task.value === "") {
+            if (this.taskDescription === "") {
                 return
             }
 
-            this.tasks.push(task.value)
-            task.value = ""
+            let newTask = {
+                description: this.taskDescription,
+                date: new Date()
+            }
+
+            this.tasks.push(newTask)
+            this.taskDescription = ""
         }
     }
 })
--- components/ToDoList.js
+++ components/ToDoList.js
@@ -10,21 +10,31 @@ app.component("todo-list", {
     `
     <div v-if="tasks.length > 0" class="box">
         <ul>
-            <li v-for="(value, index) in tasks">
+            <li v-for="(task, index) in tasks">
                 <div class="column columns is-vcentered mt-0">
                     <div class="column is-11">
                         <label class="label is-size-5">
-                            <input type="checkbox" name="{{ value }}" value="{{ value }}">
-                            {{ value }}
+                            <input type="checkbox" name="{{ task.description }}" value="{{ task.description }}">
+                            {{ task.description }}
+                            <span v-if="task.date" class="tag is-primary">Today</span>
+                            <span class="tag is-warning">Yesterday</span>
+                            <span class="tag is-danger">Erlier</span>
                         </label>
                     </div>
-                    <button @click="deleteTask(index)" class="column is-1 button is-danger is-light"><i class="fas fa-trash"></i></button>
+                    <div class="column is-1">
+                        <button @click="deleteTask(index)" class="button is-danger is-light icon"><i class="fas fa-trash"></i></button>
+                    </div>
                 </div>
                 <div v-if="tasks.length != 1 && index != tasks.length - 1" style="border-top: 3px solid #bbb;"></div>
             </li>
         </ul>
     </div>
     `,
+    data() {
+        return {
+            today: new Date(),
+        }
+    },
     methods: {
         deleteTask(index) {
             this.tasks.splice(index, 1)
