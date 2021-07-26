8919e9b03260075524de04c62bc8a95b59f51f7b
Hernán Valdés <hfvaldesg@gmail.com>
2021-07-26 17:07:26 +0200

    Start adding Chart.js to display the charts

--- client/src/components/ToDoCharts.vue
+++ client/src/components/ToDoCharts.vue
@@ -7,22 +7,36 @@
                 <button class="delete" aria-label="close" @click="closeModal"></button>
             </header>
             <section class="modal-card-body">
-            <!-- Content ... -->
+                <canvas id="myChart" width="400" height="400"></canvas>
             </section>
         </div>
     </div>
 </template>
 
 <script>
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
+import {chartData} from "../chartData.js"
+
 export default {
     name: "ToDoCharts",
     props: {
         modalState: Boolean
     },
+    data() {
+        return {
+            chartData: chartData
+        }
+    },
     methods: {
         closeModal() {
             this.$emit("closeModal")
         }
+    },
+    mounted() {
+        const ctx = document.getElementById('myChart')
+        new Chart(ctx, this.chartData)
     }
 }                
 </script>
